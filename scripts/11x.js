f411c60386fd62db0145d2b754129093afdcd6f2
DDenglizhi123 <neverforever@vip.qq.com>
2025-06-06 17:28:30 +0300

    11x added

--- scripts/11x.js
+++ scripts/11x.js
@@ -1,29 +1,31 @@
-// const todoList = [{
-//   name: 'make dinner',
-//   dueDate: '2022-12-22'
-// }, {
-//   name: 'wash dishes',
-//   dueDate: '2022-12-22'
-// }];
-const todoList = JSON.parse(localStorage.getItem('todoList'));
-renderTodoList();
+// When loading the page, load from localStorage.
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList(); //render the page with localStorage or default data;
 
 function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
-
     //const name = todoObject.name;
     //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
-    
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${i}, 1);
-        renderTodoList();
+      <button onclick="           //单击删除按你触发的事件:
+        todoList.splice(${i}, 1); //.splice 删除该下标对应的值
+        renderTodoList();         //重新渲染
+
+        // Whenever we update the todo list, save in localStorage.
+        saveToStorage();          //保存新数据到本地
       " class="delete-todo-button">Delete</button> 
     `;
     todoListHTML += html;
@@ -46,8 +48,15 @@ function addTodo() {
     name,
     dueDate
   });
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+
   inputElement.value = '';
 
   renderTodoList();
+
+  // Whenever we update the todo list, save in localStorage.
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
